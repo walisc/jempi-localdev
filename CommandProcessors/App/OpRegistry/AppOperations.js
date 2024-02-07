29f9d1890fb769f917ced9d82cffceb800d69eaa
ChidoW <warambwac@gmail.com>
2024-02-07 11:46:57 +0200

    Updating the paths

--- CommandProcessors/App/OpRegistry/AppOperations.js
+++ CommandProcessors/App/OpRegistry/AppOperations.js
@@ -1,12 +1,18 @@
 const AppUtilities = require("../../../Lib/Apps/Utils/AppUtilities")
 const BaseOperations = require("../../../Lib/Core/BaseOperations")
 const HubUtils = require("../../../Lib/Hub/Utils/HubUtilities")
+const path = require("path")
+const os = require("os")
 
 class AppOperations extends BaseOperations{
     constructor(settings, ...args){
         super(settings)
         this.utils = AppUtilities
         this.runArgs = args
+    }
+
+    GetMavenPath(){
+        return this.settings.mavenHome
     }
 
     GetRunEnv(){
--- Lib/Core/ProcessRunner.js
+++ Lib/Core/ProcessRunner.js
@@ -1,7 +1,12 @@
 const { execSync } = require('child_process');
+const path = require("path")
+const os = require("os")
+const settings = require("../../settings.json");
 
 const ProcessRunner = (command, options) => {
-    return execSync(command, options)
+    return execSync(command, {...options, env: {MAVEN_HOME: settings.mavenHome, 
+                                                JAVA_HOME:  settings.javaHome, 
+                                                ...(options?.env || {})}})
 }
 
 module.exports = ProcessRunner
